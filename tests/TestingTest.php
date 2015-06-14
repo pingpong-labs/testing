b312202dcc5d89a07d31cf90d6eb94710aedac11
Gravitano <gravitano16@gmail.com>
2015-06-14 23:18:28 +0700

    Added first test

---
+++ tests/TestingTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class TestingTest extends \Pingpong\Testing\TestCase {
+
+	public static function getLaravel()
+	{
+		return (new static)->createApplication();
+	}
+
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+	public function getBasePath()
+	{
+		return realpath(__DIR__ . '/../fixture');
+	}
+
+	public function testHomepage()
+	{
+		$this->visit('/')
+		     ->see('Laravel 5');
+	}
+}
